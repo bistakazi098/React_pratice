96fddc442d46697ac08547e47e246736d7ee4115
sagar <sagar123@gmail.com>
2023-03-04 22:15:10 +0545

    First commit

---
+++ 01-birthday-reminder/My_project1/README.md
@@ -0,0 +1,12 @@
+## Idea
+
+[https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
+
+
+## my project 1
+(1):This is my clone project of Birthday remineder project from free codecamp.
+(2):The above Project is The default project of free code camp.
+(3): To run this project and final project
+     1: Go to the project folder final or my_Project1
+     2: npm install
+     3: npm start
---
+++ 01-birthday-reminder/My_project1/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  // using useState hooks
+  // passing data object to hook
+  const [people, setPeople] = useState(data);
+
+  return <main>
+    <section className='container'>
+      <h3>{people.length} Birthday Today</h3>
+      <List people={people} />
+      <button onClick={() => setPeople([])}>Clear all</button>
+    </section>
+  </main>;
+}
+
+export default App;
